17ccd2d140e59a1d3bff60b759bbf6c1033ab588
Kristian Ferrin <129319448+Kann2411@users.noreply.github.com>
2024-12-29 09:02:14 -0500

    mailing ready

--- src/modules/email/mailing.ts
+++ src/modules/email/mailing.ts
@@ -1,7 +1,11 @@
+import { BadRequestException } from '@nestjs/common';
+import { google } from 'googleapis';
+import nodemailer from 'nodemailer';
+
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-const CLIENT_ID = process.env.CLIENT_ID;
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 
 export default async (username, email, preSubject, message) => {
   const oAuth2Client = new google.auth.OAuth2(
@@ -23,28 +27,25 @@ export default async (username, email, preSubject, message) => {
           clientId: CLIENT_ID,
           clientSecret: CLIENT_SECRET,
           refreshToken: REFRESH_TOKEN,
-          accessToken: accessToken,
+          accessToken: accessToken.token,
         },
       });
+
       const mailOptions = {
-        from: '"RoyalGames.me!!" <royalgames2025@gmail.com>', // sender address
-        to: email, // list of receivers
+        from: '"RoyalGames.me!!" <royalgames2025@gmail.com>',
+        to: email,
         subject: `${preSubject}`,
-        //text: `${message}`, // plain text body
-        html: `${message}`, // html body
+        text: `${message}`,
+        html: `${message}`,
       };
 
       const result = await transporter.sendMail(mailOptions);
       console.log('después de enviar', result);
-
       return result;
     } catch (error) {
-      throw new Error(`Error al enviar correo. ${error.message}`);
+      console.log('Error sending email', error);
+      throw new BadRequestException(`Error al enviar correo. ${error.message}`);
     }
   }
-  sendMail()
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    .then((result) => '¡Envío exitoso!')
-    .catch((error) => error.message);
-  return true;
+  return sendMail();
 };
