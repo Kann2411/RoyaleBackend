98cc26629760e38d2f53d100e12a7256d984b4ba
Kristian Ferrin <129319448+Kann2411@users.noreply.github.com>
2025-01-02 11:30:55 -0500

    synchronize false

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -13,8 +13,8 @@ const config = {
   password: process.env.DB_PASSWORD,
   autoLoadEntities: true,
   // dropSchema: true,
-  synchronize: true,
-  logging: true,
+  // synchronize: true,
+  // logging: true,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/../migrations/**/*.{js,ts}'],
 };
